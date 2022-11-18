6661c017d4cd4f05de1c7812f8e6c33001c8dabb
EDC M1PRO <cadiz.edward@gmail.com>
2022-11-18 11:21:18 -0800

    Initial Commit

---
+++ stanford-test/stanford_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_testApp.swift
+//  stanford-test
+//
+//  Created by Ed on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct stanford_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
